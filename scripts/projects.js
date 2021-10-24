3ca96bea8d3d284e8a3c816cc5e05faa849c6665
rishavchakra <rishavchakra@gmail.com>
2021-10-24 05:36:11 -0400

    CS Projects + React

--- scripts/projects.js
+++ scripts/projects.js
@@ -12,7 +12,14 @@ class Project extends React.Component {
 
 	render() {
 		return (
-			<h3>{this.title}</h3>
+			<div className="project">
+				<div className="project_title">
+					<h3>{this.title}</h3>
+					<h4>Written in {this.languages}</h4>
+				</div>
+				<p className="project_desc">{this.desc}</p>
+				<hr/>
+			</div>
 		);
 	}
 }
@@ -22,14 +29,14 @@ const cpp = 'C++'
 const rs = 'Rust'
 const py = 'Python'
 const ogl = 'OpenGL'
-const cogl = cpp + ', ' + ogl
+const cogl = cpp + ' and ' + ogl
 
 const projects = [
-	{ title: 'Raycasting engine', lang: cogl, desc: 'Raycasting description' },
-	{ title: 'BrainF Language interpreter', lang: rs, desc: 'BrainF description' },
-	{ title: 'ICESat-2 Imaging and Database tool', lang: py, desc: 'ICESat description' },
-	{ title: 'Machine Learning Image Classification', lang: py, desc: 'Convolutional Neural Network using the CIFAR-10 dataset' },
-	{ title: '2D Game Engine', lang: rs, desc: 'IN PROGRESS: A 2D Game engine with a physics system, ' },
+	{ title: 'Raycasting engine', lang: cogl, desc: '3D-replicating raycast engine, resembling Doom and Wolfenstein 3D' },
+	{ title: 'BrainF Language interpreter', lang: rs, desc: 'An interpreter for BrainF, the infamously unreadable code-golf language' },
+	{ title: 'ICESat-2 Imaging and Database tool', lang: py, desc: 'LiDAR point-cloud data science and an SQL database tool with a Tkinter GUI' },
+	{ title: 'Machine Learning Image Classification', lang: py, desc: 'Convolutional Neural Network for image recognition using the CIFAR-10 dataset' },
+	{ title: '2D Game Engine', lang: rs, desc: 'In progress: A 2D Game engine library to be used with Rust, using SDL2 windowing and WGPU graphics' },
 	{ title: 'Softbody Physics Simulation', lang: cogl, desc: 'Physics playground for softbody objects, based on a spring-mass model' }
 ]
 
