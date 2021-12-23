9560aeb3bcfcb1c8c8360c916da11f70c53084ed
Rishav <rishavchakra@gmail.com>
2021-12-23 11:38:01 -0600

    Beginning Diver implementation

---
+++ src/components/diver.js
@@ -0,0 +1,20 @@
+import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
+
+const ACCEL = 0.06
+const MAX_SPEED = 1
+
+function Diver(props) {
+    const [vel, setV] = useState(new THREE.Vector2())
+
+    useFrame(() => {
+    })
+
+    return (
+        <mesh>
+            <boxGeometry args={1, 1, 1} />
+        </mesh>
+    );
+}
+
+export default Diver;
---
+++ src/components/scene.js
@@ -0,0 +1,12 @@
+import { Canvas } from '@react-three/fiber'
+import { Diver } from './diver'
+
+function Scene() {
+    return (
+        <Canvas>
+            <Diver />
+        </Canvas>
+    );
+}
+
+export default Scene;
